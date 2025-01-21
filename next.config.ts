3b788805907ce25d92d57bd27c6dc752707792c8
Antoniel <antonielzeferino0@gmail.com>
2025-01-21 16:40:53 -0300

    update next config

--- next.config.ts
+++ next.config.ts
@@ -4,37 +4,12 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https", // Especifica o protocolo
-        hostname: "res.cloudinary.com", // Nome do host
-        pathname: "/**", // Isso permite qualquer caminho
+        protocol: "https", 
+        hostname: "res.cloudinary.com", 
+        pathname: "/**", 
       },
     ],
-  },  
-  async headers() {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          {
-            key: "Access-Control-Allow-Credentials",
-            value: "true",
-          },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, DELETE, PATCH, POST, PUT, OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization",
-          },
-        ],
-      },
-    ];
-  },
+  }
 };
 
 export default nextConfig;
